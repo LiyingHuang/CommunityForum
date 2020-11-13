c7555250e14f2bd2c8e62ffc542bf834bbd9162b
liying <liyinghuang@Liying-MacBook-Pro.local>
2020-11-13 16:01:00 -0500

    create the project with spring boot

---
+++ src/main/java/com/coral/community/CommunityApplication.java
@@ -0,0 +1,20 @@
+package com.coral.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityApplication.class, args);
+	}
+
+}
+
+/*
+*  1. when we run the main method, springboot help us start the tomcat sever in the bottom
+*  2. spring boot contains a tomcat jar package, we use this embedded tomcat
+*  3. tomcat: sever
+**/
